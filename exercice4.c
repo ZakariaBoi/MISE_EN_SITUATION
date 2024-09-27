a65b039c74869348dc2db04b2f50724c2de2149f
Zakaria Boikouta <mr.zak.boi@gmail.com>
2024-09-27 17:26:19 +0200

    Correction

--- exercice4.c
+++ exercice4.c
@@ -7,17 +7,17 @@ int main(){
         scanf("%f",&temps[i]);
     }
     float min = temps[0];
+    float max = temps[0];
+
     for(int i = 0; i <= 4; i++){
         if(temps[i] < min){
             min = temps[i];
         }
-    }
-    float max = temps[0];
-    for(int i = 0; i <= 4; i++){
-        if(temps[i] > max){
+        else if(temps[i] > max){
             max = temps[i];
         }
     }
+  
     printf("Sortie : \n");
     printf("La Plus Haute Temperatue : %.1f\n",max);
     printf("La Plus basse Temperatue : %.1f\n",min);
